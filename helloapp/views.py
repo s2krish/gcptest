2dbf0c8ca823d08bd6fda7abfeba7be5b7016c5d
Krish <krishna.sunuwar@gmail.com>
2022-05-11 14:00:09 +0545

    deployment.yaml added

--- helloapp/views.py
+++ helloapp/views.py
@@ -1,8 +1,18 @@
+import json
 import datetime
 from django.http import HttpResponse
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def health(request):
     now = datetime.datetime.utcnow()
     html = f"Time is: {now}"
+
+    data = request.body.decode('utf-8')
+
+    if data:
+        html = f"{html}\n\nPayload: {data}"
+
+    print (html)
+
     return HttpResponse(html)
